f49f0e4a1eb1be74f1a04ab9c58db16fb6dcc87d
eddianvasquez <eddian_vasquez@ucne.edu.do>
2026-03-04 19:40:35 -0400

    agregando Contenido al Appmodule

---
+++ app/src/main/java/edu/ucne/eddian_vasquez_ap2_p2/di/appmodule.kt
@@ -0,0 +1,24 @@
+package edu.ucne.eddian_vasquez_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+}
